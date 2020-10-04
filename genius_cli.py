69a17fc73cb15d96294caa1b3cacd6e4b9198e7e
Isaac <iware@wisc.edu>
2020-10-04 18:12:36 -0500

    Prints lyrics

--- genius_cli.py
+++ genius_cli.py
@@ -2,23 +2,32 @@ import config
 
 import json
 import requests
+from bs4 import BeautifulSoup
+import bs4
 
 base_url = "https://api.genius.com"
 headers = {'Authorization': 'Bearer ' + config.access_token}
+
+web_url = "https://genius.com"
 
 search_url = base_url + "/search"
 song_title = "Humble"
 artist_name = "Kendrick Lamar"
 params = {'q': song_title}
 
-def get_url_path_from_song_api_path(song_api_path):
+def get_lyrics_from_song_api_path(song_api_path):
     song_url = base_url + song_api_path
     response = requests.get(song_url, headers=headers)
     r_json = response.json()
-    print(r_json["response"]["song"]["path"])
-
+    page_path = r_json["response"]["song"]["path"]
+    page_url = web_url + page_path
+    page = requests.get(page_url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    soup = soup.find(class_="lyrics")
+    lyrics = soup.get_text()
+    return lyrics
+    
 response = requests.get(search_url, params=params, headers=headers)
-
 response_json = response.json()
 
 # Parse search results for matching artist
@@ -31,4 +40,5 @@ for hit in response_json["response"]["hits"]:
 # Parse for song api_path
 if song_info:
     api_path = song_info["result"]["api_path"]
-    get_url_path_from_song_api_path(api_path)
+    lyrics = get_lyrics_from_song_api_path(api_path)
+    print(lyrics)
